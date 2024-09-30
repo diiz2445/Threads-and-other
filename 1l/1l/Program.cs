527cd1d8e18b2ffc32cdd41fa3788712463d6061
Alex <ax90790@gmail.com>
2024-09-30 13:20:03 +0500

    Добавьте файлы проекта.

--- 1l/1l/Program.cs
+++ 1l/1l/Program.cs
@@ -1,8 +1,48 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
 
+class Program
+{
+    static void Main()
+    {
+        //Console.ReadLine();
+        int n = 5; // Количество строк
+        int m = 5; // Количество столбцов
+        int[,] matrix = new int[n, m];
+
+        // Создаем массив потоков
+        Thread[] threads = new Thread[n];
+
+        // Запускаем потоки для заполнения строк матрицы
+        for (int i = 0; i < n; i++)
+        {
+            int rowIndex = i; // Локальная переменная для использования в потоке
+            threads[i] = new Thread(() => FillRow(matrix, rowIndex, m));
+            threads[i].Start();
+        }
+
+        // Ждем завершения всех потоков
+        for (int i = 0; i < n; i++)
+        {
+            threads[i].Join();
+        }
+
+        // Вывод матрицы на экран
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void FillRow(int[,] matrix, int rowIndex, int m)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[rowIndex, j] = rowIndex * m + j; // Пример заполнения
+        }
     }
 }
---
+++ 1l/1l/thread_winAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _1l
+{
+    internal class thread_winAPI
+    {
+        
+
+    }
+}
