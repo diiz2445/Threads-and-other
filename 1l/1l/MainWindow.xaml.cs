b1a1783d480cebfa39e775749006332bb3ee9322
Alex <ax90790@gmail.com>
2024-10-05 23:33:40 +0500

    вывод начальной матрицы в окно#1

--- 1l/1l/MainWindow.xaml.cs
+++ 1l/1l/MainWindow.xaml.cs
@@ -13,9 +13,6 @@ using System.Windows.Shapes;
 
 namespace _1l
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
