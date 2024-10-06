0c58bdc87b7500fe3ed459329d15b1a78dcbeb1f
Alex <ax90790@gmail.com>
2024-10-06 14:56:36 +0500

    вывод матриц, функция умножения, сортировка матрицдобавил поля ввода матрицы

--- 1l/1l/MainWindow.xaml.cs
+++ 1l/1l/MainWindow.xaml.cs
@@ -96,67 +96,8 @@ namespace _1l
             MatrixDataGrid_multi.Visibility = Visibility.Visible;
 
         }
-        private void CreateColumns_multiply(List<List<double>> matrix)
-        {
-            if (matrix.Count == 0) return;
-
-            MatrixDataGrid_multi.Columns.Clear(); // очищаем предыдущие столбцы, чтобы избежать их дублирования
-
-            for (int i = 0; i < matrix[0].Count; i++) // изменил цикл на количество столбцов
-            {
-                var column = new DataGridTextColumn
-                {
-                    Binding = new Binding($"[{i}]")
-                };
-                MatrixDataGrid_multi.Columns.Add(column);
-            }
-
-            MatrixDataGrid_multi.ItemsSource = matrix;
-        }
-        private void CreateColumns_sort(List<List<double>> matrix)
-        {
-            if (matrix.Count == 0) return;
-
-            MatrixDataGrid_sorted.Columns.Clear(); // очищаем предыдущие столбцы, чтобы избежать их дублирования
-
-            for (int i = 0; i < matrix[0].Count; i++) // изменил цикл на количество столбцов
-            {
-                var column = new DataGridTextColumn
-                {
-                    Binding = new Binding($"[{i}]")
-                };
-                MatrixDataGrid_sorted.Columns.Add(column);
-            }
-
-            MatrixDataGrid_sorted.ItemsSource = matrix;
-        }
-        private void GenerateMatrix_Click(object sender, RoutedEventArgs e)
-        {
-            // Получаем значения из полей ввода
-            int rows = int.Parse(RowsInput.Text);       // количество строк
-            int columns = int.Parse(ColumnsInput.Text); // количество столбцов
-            int threads = int.Parse(ThreadsInput.Text); // количество потоков (можно не использовать в примере)
-
-            matrix_list = new List<List<double>>();
-            matrix = Threads.fill_matrix(rows, columns, threads);
-            matrix_list = MatrixViewModel.GenerateMatrix(RowsInput.Text, ColumnsInput.Text, ThreadsInput.Text);
-           
-            MatrixDataGrid.CanUserAddRows = false; // отключаем возможность добавления новой строки
-            CreateColumns(matrix_list);
-
-            MatrixDataGrid.ItemsSource = matrix_list;
-            MatrixDataGrid_sorted.ItemsSource= MatrixViewModel.multiply_list(RowsInput.Text, ColumnsInput.Text, ThreadsInput.Text,Multiply.Text);
-            MatrixDataGrid.Visibility = Visibility.Visible;
-        }
-
-        private void multiply_Click(object sender, RoutedEventArgs e)
-        {
-            MatrixDataGrid.CanUserAddRows = false; // отключаем возможность добавления новой строки
-            CreateColumns_multiply(matrix_list);
-
-            MatrixDataGrid_multi.ItemsSource = MatrixViewModel.multiply_list(matrix, Multiply.Text);
-            MatrixDataGrid_multi.Visibility = Visibility.Visible;
-
-        }
+        
+        
+        
     }
 }
