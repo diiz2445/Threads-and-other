82b1fdd63835fe4f69a1aa3329c3a4218b1b3161
Alex <ax90790@gmail.com>
2024-10-26 18:35:03 +0500

    1,2 done

--- 3lab/3lab/3lab/Program.cs
+++ 3lab/3lab/3lab/Program.cs
@@ -1,7 +1,40 @@
-﻿internal class Program
+﻿namespace _3lab
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        
-    }
+        private static void Main(string[] args)
+        {
+            string mutexName = "Global\\MyNamedMutex"; // Имя мьютекса
+
+            // Открытие или создание мьютекса по имени
+            using (Mutex mutex = new Mutex(false, mutexName, out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    Console.WriteLine("Мьютекс создан текущим приложением.");
+                }
+                else
+                {
+                    Console.WriteLine("Мьютекс уже существует. Ожидаем его освобождения.");
+                }
+
+                // Попробуем заблокировать мьютекс
+                mutex.WaitOne();
+                Console.WriteLine("Мьютекс захвачен. Работа с ресурсом...");
+
+                // Имитация работы
+                Thread.Sleep(3000);
+
+                // Освобождение мьютекса
+                Console.WriteLine("Освобождаем мьютекс.");
+                mutex.ReleaseMutex();
+            }
+
+            Console.WriteLine("Завершено.");
+
+            Console.ReadKey();
+
+        }
+
+    } 
 }
